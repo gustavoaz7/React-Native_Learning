4a8b53af8360d2dcf490866e8007fd6e040d07e7
gustavoaz7 <gustavo_azevedo_7@hotmail.com>
2018-03-09 19:15:11 -0300

    Add data & style

--- albums/src/components/AlbumDetails.js
+++ albums/src/components/AlbumDetails.js
@@ -1,15 +1,59 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View, Image } from 'react-native'
 
 import Card from './Card'
 import CardSection from './CardSection'
 
-const AlbumDetail = (props) => (
-    <Card>
-        <CardSection>
-            <Text>{props.album.title}</Text>
-        </CardSection>
-    </Card>
-)
+const AlbumDetail = (props) => {
+    const { thumbnail_image, title, artist, image } = props.album
+    
+    return (
+        <Card>
+            <CardSection>
+                <View style={styles.thumbnailContainer} >
+                    <Image 
+                        source={{ uri: thumbnail_image }} 
+                        style={styles.thumbnail} 
+                    />
+                </View>
+                <View style={styles.textContainer} >
+                    <Text style={styles.title} >{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardSection>
+            <CardSection>
+                <Image 
+                    source={{ uri: image }} 
+                    style={styles.image}    
+                />
+            </CardSection>
+        </Card>
+    )
+}  
+
+const styles = {
+    title: {
+        fontSize: 18
+    },
+    textContainer: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    thumbnail: {
+        height: 50,
+        width: 50
+    },
+    thumbnailContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    image: {
+        height: 300,
+        flex: 1,
+        width: null
+    }
+}
 
 export default AlbumDetail
