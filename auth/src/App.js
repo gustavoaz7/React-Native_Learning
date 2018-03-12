37ed5a815f129a27dc42d89ee8069b27ab7ca845
gustavoaz7 <gustavo_azevedo_7@hotmail.com>
2018-03-12 15:00:36 -0300

    Add login/logout logic

--- auth/src/App.js
+++ auth/src/App.js
@@ -3,20 +3,45 @@ import { View } from 'react-native'
 import firebase from 'firebase'
 
 const firebaseConfig = require('../firebaseConfig')
-import { Header } from './components/common'
+import { Header, Button, Spinner, CardSection } from './components/common'
 import LoginForm from './components/LoginForm'
 
 class App extends Component {
+    state = {
+        loggedIn: null
+    }
 
     componentWillMount() {
         firebase.initializeApp(firebaseConfig)
+
+        firebase.auth().onAuthStateChanged(user => {
+           user ? this.setState({ loggedIn: true }) : this.setState({ loggedIn: false })
+        })
+    }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                    <CardSection>
+                        <Button 
+                            content='Log out'
+                            onPress={() => firebase.auth().signOut()}
+                        />
+                    </CardSection>
+                )
+            case false: 
+                return <LoginForm />
+            default:
+                return <Spinner size='large' />
+        }
     }
 
     render() {
         return (
             <View>
                 <Header content='Authentication' />
-                <LoginForm />
+                {this.renderContent()}
             </View>
         )
     }
