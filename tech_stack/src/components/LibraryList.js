aadef892a7c20858a96f2aef9a6ea51f91bd52a9
gustavoaz7 <gustavo_azevedo_7@hotmail.com>
2018-03-12 18:51:07 -0300

    Use FlatList (ListView deprecated)

--- tech_stack/src/components/LibraryList.js
+++ tech_stack/src/components/LibraryList.js
@@ -1,29 +1,21 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { ListView } from 'react-native'
+import { FlatList } from 'react-native'
 
 import ListItem from './ListItem'
 
 class LibraryList extends Component {
-
-    componentWillMount() {
-        const ds = new ListView.DataSource({
-            rowHasChanged: (r1, r2) => r1 !== r2
-        })
-
-        this.dataSource = ds.cloneWithRows(this.props.libraries)
-    }
 
     renderRow(library) {
         return <ListItem library={library} />
     }
 
     render() {
-        console.log(this.props)
         return (
-            <ListView 
-                dataSource={this.dataSource}
-                renderRow={this.renderRow}
+            <FlatList
+                data={this.props.libraries}
+                renderItem={({ item }) => this.renderRow(item)}
+                keyExtractor={item => `library ${item.id}`}
             />
         )
     }
