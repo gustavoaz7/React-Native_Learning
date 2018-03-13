71142c38d9d4c8b3549d42ffc4341b67cfdeb8f9
gustavoaz7 <gustavo_azevedo_7@hotmail.com>
2018-03-13 10:11:10 -0300

    Add common components

---
+++ manager/src/components/common/Button.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Text, TouchableOpacity } from 'react-native'
+
+const Button = (props) => (
+    <TouchableOpacity 
+        style={styles.button} 
+        onPress={props.onPress}
+    >
+        <Text style={styles.text} >
+            {props.content}
+        </Text>
+    </TouchableOpacity>
+)
+
+const styles = {
+    button: {
+        flex: 1,
+        alignSelf: 'stretch',
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#007aff',
+        marginLeft: 5,
+        marginRight: 5
+    },
+    text: {
+        alignSelf: 'center',
+        color: '#007aff',
+        fontSize: 16,
+        fontWeight: '600',
+        paddingTop: 10,
+        paddingBottom: 10
+    }
+}
+
+export { Button }
---
+++ manager/src/components/common/index.js
@@ -0,0 +1,6 @@
+export * from './Button'
+export * from './Card'
+export * from './CardSection'
+export * from './Header'
+export * from './Field'
+export * from './Spinner'
